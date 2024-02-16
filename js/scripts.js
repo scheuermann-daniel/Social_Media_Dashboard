5a12f5fe8aa11f0eb4e18af513b43559cd0ef3cb
scheuermann-daniel <scheuermannpauldaniel@gmail.com>
2024-02-15 22:03:19 -0500

    Added graph visualization of follower count

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,28 @@ function closeSidebar() {
     sidebarOpen = false;
   }
 }
+
+google.charts.load('current',{packages:['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+// Set Data
+const data = google.visualization.arrayToDataTable([
+  ['Price', 'Size'],
+  [50,7],[60,8],[70,8],[80,9],[90,9],
+  [100,9],[110,10],[120,11],
+  [130,14],[140,14],[150,15]
+]);
+
+// Set Options
+const options = {
+  hAxis: {title: 'Square Meters'},
+  vAxis: {title: 'Price in Millions'},
+  legend: 'none'
+};
+
+// Draw
+const chart = new google.visualization.LineChart(document.getElementById('myChart'));
+chart.draw(data, options);
+}
